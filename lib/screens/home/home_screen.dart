fd36f70b423087a5e2bfcf9024be149d350adc8e
Aidar Khabibullin <aydar.kh@gmail.com>
2024-05-07 01:30:15 +0500

    refactor: build home screen structure and add widgets

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:avia_tickets/app/style/extensions.dart';
 import 'package:avia_tickets/data/local_storage_repository.dart';
 import 'package:avia_tickets/data/ticket_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({
@@ -20,6 +21,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   late final TicketRepository _ticketRepository;
   late final LocalStorageRepository _localStorageRepository;
+  static const _vGap32 = SizedBox(height: 32);
 
   @override
   void initState() {
@@ -30,9 +32,295 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox.expand(
-      child: ColoredBox(
-        color: context.appColors.black,
+    return const SafeArea(
+      child: Scaffold(
+        body: SizedBox(
+          width: double.infinity,
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  _HeaderText(),
+                  _vGap32,
+                  _SearchTicketsBlock(),
+                  _vGap32,
+                  _OffersHorizontalListView(),
+                  _vGap32,
+                  _ShowAllOffersButton(),
+                  _vGap32,
+                  _SuggestionsBlock(),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _SuggestionsBlock extends StatelessWidget {
+  const _SuggestionsBlock();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Ваш первый раз',
+          textAlign: TextAlign.start,
+          style: context.appTextStyles.title1,
+        ),
+        const SizedBox(height: 12),
+        SizedBox(
+          height: 230,
+          child: ListView.separated(
+            scrollDirection: Axis.horizontal,
+            itemCount: 5,
+            itemBuilder: (context, index) {
+              return const SizedBox(
+                height: 230,
+                width: 132,
+                child: Placeholder(),
+              );
+            },
+            separatorBuilder: (context, index) => const SizedBox(width: 16),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _ShowAllOffersButton extends StatelessWidget {
+  const _ShowAllOffersButton();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 42,
+      width: double.infinity,
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          color: context.appColors.grey4,
+          borderRadius: const BorderRadius.all(Radius.circular(8)),
+        ),
+        child: Center(
+          child: Text(
+            'Показать все места',
+            style: context.appTextStyles.buttonText,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _OffersHorizontalListView extends StatelessWidget {
+  const _OffersHorizontalListView();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Музыкально отлететь',
+          textAlign: TextAlign.start,
+          style: context.appTextStyles.title1,
+        ),
+        const SizedBox(height: 12),
+        SizedBox(
+          height: 230,
+          child: ListView.separated(
+            shrinkWrap: true,
+            scrollDirection: Axis.horizontal,
+            physics: const PageScrollPhysics(),
+            itemBuilder: (BuildContext context, int index) {
+              return const _OfferCard(
+                image: 'image',
+                town: 'town name',
+                title: 'title name',
+                price: 1000,
+              );
+            },
+            separatorBuilder: (BuildContext context, int index) {
+              return const SizedBox(width: 20);
+            },
+            itemCount: 3,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _OfferCard extends StatelessWidget {
+  const _OfferCard({
+    required this.image,
+    required this.title,
+    required this.town,
+    required this.price,
+  });
+
+  final String image;
+  final String title;
+  final String town;
+  final int price;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 132,
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          color: context.appColors.black,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 132,
+              width: 132,
+              child: Placeholder(
+                child: Center(child: Text(image)),
+              ),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              title,
+              style: context.appTextStyles.title3,
+            ),
+            const SizedBox(height: 8),
+            Text(
+              town,
+              style: context.appTextStyles.text2,
+            ),
+            const SizedBox(height: 8),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: SvgPicture.asset('assets/icons/svg/airplane.svg'),
+                ),
+                Text(
+                  'от $price руб.',
+                  style: context.appTextStyles.text2,
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _HeaderText extends StatelessWidget {
+  const _HeaderText();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 200,
+      height: 100,
+      child: Center(
+        child: Text(
+          'Поиск дешевых авиабилетов',
+          textAlign: TextAlign.center,
+          style: context.appTextStyles.title1,
+        ),
+      ),
+    );
+  }
+}
+
+class _SearchTicketsBlock extends StatelessWidget {
+  const _SearchTicketsBlock();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 122,
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          color: context.appColors.grey3,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: DecoratedBox(
+            decoration: BoxDecoration(
+                color: context.appColors.grey4,
+                borderRadius: BorderRadius.circular(12),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black12,
+                    blurRadius: 8,
+                    offset: Offset(0, 4),
+                  ),
+                ]),
+            child: Row(
+              children: [
+                const SizedBox(width: 16),
+                SizedBox(
+                  width: 24,
+                  height: double.infinity,
+                  child: FittedBox(
+                    child: SvgPicture.asset(
+                      'assets/icons/svg/magnifier.svg',
+                      semanticsLabel: 'Search icon',
+                      colorFilter: ColorFilter.mode(
+                        context.appColors.white,
+                        BlendMode.srcIn,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 24),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          decoration: const InputDecoration(
+                            hintText: 'Откуда - Москва',
+                          ),
+                          style: context.appTextStyles.buttonText.copyWith(
+                            color: context.appColors.white,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                      Divider(
+                        height: 1,
+                        thickness: 1,
+                        color: context.appColors.grey5,
+                      ),
+                      Expanded(
+                        child: TextField(
+                          decoration: const InputDecoration(
+                            hintText: 'Куда - Турция',
+                          ),
+                          style: context.appTextStyles.buttonText.copyWith(
+                            color: context.appColors.white,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(width: 16),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
