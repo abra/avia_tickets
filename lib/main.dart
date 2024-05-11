ce79cab184a5712c85d80d04b7145967d7fd4250
Aidar Khabibullin <aydar.kh@gmail.com>
2024-05-12 02:30:25 +0500

    Switch to Mock Api while dev

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'app.dart';
 import 'data/local_storage/shared_prefs_storage.dart';
 import 'data/local_storage.dart';
+import 'data/service/mock_api_service.dart';
 import 'data/service/ticket_api_service.dart';
 import 'data/ticket_repository.dart';
 
@@ -11,7 +12,8 @@ void main() {
   WidgetsFlutterBinding.ensureInitialized();
   final dio = Dio(BaseOptions(contentType: "application/json"));
   final ticketRepository = TicketRepository(
-    ticketApiService: TicketApiService(dio),
+    // ticketApiService: TicketApiService(dio),
+    ticketApiService: MockApiService(),
   );
   final localStorageRepository = LocalStorage(
     storage: SharedPrefsStorage(),
